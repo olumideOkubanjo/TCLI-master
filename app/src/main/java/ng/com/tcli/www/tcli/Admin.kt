7e4280cc4daaacf37e038250985e97da6f920a77
Tobi Okubanjo <olumide.okubanjo@gmail.com>
2019-02-06 12:43:02 -0700

    Update 1.0.1 Continue Listener on ProfilePage Fixed. Admin Page created

---
+++ app/src/main/java/ng/com/tcli/www/tcli/Admin.kt
@@ -0,0 +1,12 @@
+package ng.com.tcli.www.tcli
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Admin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
